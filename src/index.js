459620edddbf9492065bb853f5a178e2a2850f65
DeepakSED <deepakcegin@gmail.com>
2022-07-25 13:21:05 +0530

    FileDone

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose =require("mongoose");
+const route = require("./route/route");
+const multer = require("multer")
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(multer().any());
+
+mongoose.connect("mongodb+srv://Birappa:MangoDB@cluster0.m5phg.mongodb.net/group15Database",{
+    useNewUrlParser : true,
+})
+.then(()=>console.log("mongoDB Connected"))
+.catch((err)=>console.log(err));
+
+app.use("/",route);
+
+app.listen(process.env.PORT || 3000, function (){
+    console.log("Express app running on port " +(process.env.PORT || 3000));
+})
---
+++ src/models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    fname: { type: String, required: true, trim: true },
+    lname: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    profileImage: { type: String, required: true },
+    phone: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true, trim: true, min: 8, max: 15 },
+    address: {
+      shipping: {
+        street: { type: String, required: true, trim: true },
+        city: { type: String, required: true, trim: true },
+        pincode: { type: Number, required: true, trim: true },
+      },
+      billing: {
+        street: { type: String, required: true, trim: true },
+        city: { type: String, required: true, trim: true },
+        pincode: { type: Number, required: true, trim: true },
+      },
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("user", userSchema);
