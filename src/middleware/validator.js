96717ae39fb09131a84a4fce84e3e66bd917db38
DeepakSED <deepakcegin@gmail.com>
2022-07-26 10:45:15 +0530

    validation

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -20,9 +20,9 @@ const validFileRegex = /^.+\.(?:(?:[dD][oO][cC][xX]?)|(?:[pP][dD][fF])|(?:[pP][n
 
 
 const nameRegex = /^[a-zA-Z ]{2,30}$/
-const passwordRegex = /^(?=.[0-9])(?=.[A-Z])(?=.[a-z])(?=.[!@#$%^&?%|])[a-zA-Z0-9!@#$%^&?%|]{6,16}$/
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$/;
 const phoneRegex =  /^((\+91)?|91)?[6789][0-9]{9}$/;
-const pincodeRegex = /^[1-9]{1}[0-9]{2}\\s{0, 1}[0-9]{3}$/
+const pincodeRegex = /^[0-9]{6}$/
 const emailRegex = /^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/
 
 module.exports = {isValid,isValidFiles,isValidRequestBody,validFileRegex,nameRegex,passwordRegex,phoneRegex,pincodeRegex,emailRegex};
