7ca4b47ddc295b6f045313240d377f9dafa49941
DeepakSED <deepakcegin@gmail.com>
2022-07-26 13:26:36 +0530

    Feature1

--- src/route/route.js
+++ src/route/route.js
@@ -1,7 +1,16 @@
 const express = require("express");
-const { createUser } = require("../controllers/uesrController");
+const { createUser,loginUser, getUser, updateUser} = require("../controllers/uesrController");
+const { authorization,authentication } = require("../middleware/Authentication");
+
 const router = express.Router();
 
+
+//Feature 1 User ApIs 
 router.post("/register",createUser);
+router.post("/login",loginUser);
+router.get("/user/:userId/profile",authentication,authorization,getUser);
+router.put("/user/:userId/profile",authentication,authorization,updateUser)
+
+//Feature 2 User ApIs 
 
 module.exports =router;
